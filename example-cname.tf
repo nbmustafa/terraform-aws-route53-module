5525a9a184170af01afec98085beb27c2b54886f
Nashwan Bilal <nashwan.bilal@gmail.com>
2020-12-12 22:10:41 +1100

    updated module

--- example-cname.tf
+++ example-cname.tf
@@ -1,7 +1,8 @@
 
 ## Example creating CNAMe with weighted routing policy enabled: 
 module "service_dns_cname" {
-  source = "./modules/"
+  source  = "nashvan/route53-module/aws"
+  version = "1.1.0"
 
   hosted_zone = local.account_config["hosted_zone"]
   
