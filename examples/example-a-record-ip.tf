9a7c1f21b2afabeec72a2e9e9228099088f70719
nashwan <nashwan.bilal@gmail.com>
2020-12-12 22:23:23 +1100

    updated module path

--- examples/example-a-record-ip.tf
+++ examples/example-a-record-ip.tf
@@ -2,7 +2,7 @@
 ## Example creating A record with IP: 
 module "service_dns" {
   source  = "nashvan/route53-module/aws"
-  version = "1.1.0"
+  version = "1.2.0"
 
   hosted_zone = local.account_config["hosted_zone"]
   
--- examples/example-a-record.tf
+++ examples/example-a-record.tf
@@ -2,7 +2,7 @@
 ## Example creating A record with Alias: 
 module "service_dns_a_record" {
   source  = "nashvan/route53-module/aws"
-  version = "1.1.0"
+  version = "1.2.0"
 
   hosted_zone = local.account_config["hosted_zone"]
   
